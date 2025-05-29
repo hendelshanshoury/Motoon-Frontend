dfd468c7de06009f0e00598cae607d6fe7ad2f1c
Hend Elsayed <eng.hendelshanshoury@gmail.com>
2025-05-29 23:42:26 +0300

    refactor: Clean up markup in OurVision component for improved readability

--- src/domains/about/components/OurVision.tsx
+++ src/domains/about/components/OurVision.tsx
@@ -4,15 +4,13 @@ import vsion from "../../../assets/images/our-vision-04871090.png"; // Adjust th
 export const OurVision = () => {
   return (
     <>
-      <div className="container relative py-18 mx-auto md:px-12 lg:px-28 bg-[#F5F9FF]">
+      <div className=" relative py-18 mx-auto md:px-12 lg:px-28 bg-[#F5F9FF]">
         <div className="absolute right-0 hidden top-10 lg:block">
           <img src={background} alt="background" className="w-[65%] " />
         </div>
 
         <div className="flex flex-col items-center gap-8 px-3 py-8 mx-auto lg:flex-row ">
-          <div
-            className="md:w-[50%] flex items-center md:justify-end w-[150%] justify-center py-4"
-          >
+          <div className="md:w-[50%] flex items-center md:justify-end w-[150%] justify-center py-4">
             <img src={vsion} alt="motonphone" className="w-full" />
           </div>
           <div className="flex flex-col items-center justify-center gap-4 md:items-start md:justify-start lg:w-[50%]  px-4">
